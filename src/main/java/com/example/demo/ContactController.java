094265f46a3161b96786434b1ceb6ee3ccafbdcc
SHOTA <subaseball820@gmail.com>
2025-05-22 18:44:12 +0900

    first commit

---
+++ src/main/java/com/example/demo/ContactController.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import com.example.demo.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class ContactController {
+
+    @GetMapping("/")
+    public String contact(Model model) {
+        User user = new User();
+        user.setName("田中太郎");
+        user.setEmail("tanaka.taro@example.com");
+        user.setPhoneNumber("000-000-0000");
+
+        List<String> inquiries = List.of("◯◯◯の問い合わせ", "×××の問い合わせ", "△△△の問い合わせ");
+        List<String> menu = List.of("TOP", "ABOUT", "CONTACT");
+
+        model.addAttribute("user", user);
+        model.addAttribute("inquiryTypes", inquiries);
+        model.addAttribute("menuLinks", menu);
+        return "contact";
+    }
+}
+
